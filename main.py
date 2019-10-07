de39f4437bf758341fafc282cccdfe2d461d8a49
Travis Brackney <tbrackney@gmail.com>
2019-10-06 20:56:13 -0700

    finished exercise

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+"""
+Project Name: github-latest
+File Name: main.py
+Author: Travis Brackney
+Class: Python 230 - Self paced online
+Date Created 10/6/2019
+Python Version: 3.7.2
+"""
+
+
 import sys
 import json
 
@@ -6,6 +17,16 @@ import requests
 # Use Like python githubber.py JASchilz
 # (or another user name)
 
+
+def get_event_time(username):
+    base_url = "https://api.github.com/"
+    events_url = f"{base_url}users/{username}/events"
+
+    response = requests.get(events_url)
+    parsed = json.loads(response.text)
+    return parsed[0]['created_at']
+
+
 if __name__ == "__main__":
     username = sys.argv[1]
 
@@ -13,8 +34,6 @@ if __name__ == "__main__":
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
+    timestamp = get_event_time(username)
 
-    print("COMPLETE THE TODOs")
-    
-
-
+    print(timestamp)
